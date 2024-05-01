428b624899f07603f62a671cb39e744295cee307
richard523 <ricgat523@gmail.com>
2024-04-30 20:32:17 -0500

    rep6

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://rep6.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Hey</span>
+        <span> Rep6</span>
       </a>
     </span>
   );
